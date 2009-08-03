2362f5055172e0b41823b8c182ab536aaef96443
Mark Young <zarzax@gmail.com>
2009-08-03 16:24:01 -0500

    Added additional validations.

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,3 +1,9 @@
 class Project < ActiveRecord::Base
   validates_presence_of :name, :description, :image_url
+  validates_uniqueness_of :name
+  validates_format_of :image_url
+                      :with     => %r{\.(gif|jpg|png)$}i,
+                      :message  => 'must be a URL for GIF, JPG, or PNG image.'
+                      
+                      
 end
