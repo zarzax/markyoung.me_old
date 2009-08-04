adf17c5ad197be94295b8546e70e4217d7995799
Mark Young <zarzax@gmail.com>
2009-08-04 00:55:35 -0500

    Added validation and a migration with test data. Also a default image.

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,7 +1,7 @@
 class Project < ActiveRecord::Base
   validates_presence_of :name, :description, :image_url
   validates_uniqueness_of :name
-  validates_format_of :image_url
+  validates_format_of :image_url,
                       :with     => %r{\.(gif|jpg|png)$}i,
                       :message  => 'must be a URL for GIF, JPG, or PNG image.'
                       
--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20090803211414) do
+ActiveRecord::Schema.define(:version => 20090804054632) do
 
   create_table "projects", :force => true do |t|
     t.string   "name"
