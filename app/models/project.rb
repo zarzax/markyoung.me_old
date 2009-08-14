0425a0ae84e27864c316d73c3de7e8d845f20184
Mark Young <zarzax@gmail.com>
2009-08-14 17:02:12 -0500

    Installed the acts_as_taggable_on plugin. 	* Modified the project model to use plugin 	* Modified the project views to use model

--- app/models/project.rb
+++ app/models/project.rb
@@ -5,7 +5,7 @@ class Project < ActiveRecord::Base
                       :with     => %r{\.(gif|jpg|png)$}i,
                       :message  => 'must be a URL for GIF, JPG, or PNG image.'
   
-  has_many :catagories
+  acts_as_taggable_on :languages, :deliverables
 
   def self.find_projects
     find(:all, :order => 'name')
--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20090804054632) do
+ActiveRecord::Schema.define(:version => 20090814193459) do
 
   create_table "projects", :force => true do |t|
     t.string   "name"
@@ -18,6 +18,24 @@ ActiveRecord::Schema.define(:version => 20090804054632) do
     t.string   "image_url"
     t.datetime "created_at"
     t.datetime "updated_at"
+  end
+
+  create_table "taggings", :force => true do |t|
+    t.integer  "tag_id"
+    t.integer  "taggable_id"
+    t.integer  "tagger_id"
+    t.string   "tagger_type"
+    t.string   "taggable_type"
+    t.string   "context"
+    t.datetime "created_at"
+  end
+
+  add_index "taggings", ["tag_id"], :name => "index_taggings_on_tag_id"
+  add_index "taggings", ["taggable_id", "taggable_type", "context"], :name => "index_taggings_on_taggable_id_and_taggable_type_and_context"
+
+  create_table "tags", :force => true do |t|
+    t.string  "name"
+    t.integer "count", :default => 0
   end
 
 end
